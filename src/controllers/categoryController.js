71457c92c8754c16c949ecf51359b6204408476a
amritansh333 <153156600+amritansh333@users.noreply.github.com>
2026-03-07 00:14:45 +0530

    removed category sort by name

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -2,7 +2,7 @@ import Category from "../models/Category.js";
 
 export const getAllCategories = async (req, res) => {
   try {
-    const categories = await Category.find().sort({ name: 1 });
+    const categories = await Category.find();
     res.json(categories);
   } catch (error) {
     res.status(500).json({ message: error.message });
