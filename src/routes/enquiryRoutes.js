813f9f6c9e931dbc006eaf0bcb4d0982c5036648
amritansh333 <153156600+amritansh333@users.noreply.github.com>
2026-04-12 04:51:41 +0530

    added enquiry - contact form data

--- src/routes/enquiryRoutes.js
+++ src/routes/enquiryRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import mongoose from "mongoose";
-import Enquiry from "../models/Enquiry.js";
+import { createEnquiry } from "../controllers/enquiryController.js";
 
 
 const router = express.Router();
-const { createEnquiry } = require("../controllers/enquiryController");
+
 
 router.post("/", createEnquiry);
 
